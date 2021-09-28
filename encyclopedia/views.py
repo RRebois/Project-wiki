6763d87d8874a9bfaa50e8e17d05c4118300bc94
Rolando Rebois <reboisr@eduroam-083154.grenet.fr>
2021-09-28 11:54:20 +0200

    FInished the New page feature

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -2,12 +2,25 @@ from django.shortcuts import render, redirect
 from django import forms
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.contrib import messages
 
 from . import util
 
+import markdown2
+markdown2.markdown("*boo!*")  # or use `html = markdown_path(PATH)`
+u'<p><em>boo!</em></p>\n'
+
+from markdown2 import Markdown
+markdowner = Markdown()
+markdowner.convert("*boo!*")
+u'<p><em>boo!</em></p>\n'
+markdowner.convert("**boom!**")
+u'<p><strong>boom!</strong></p>\n'
+
+
 class NewPageForm(forms.Form):
-    title = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder': 'title'}))
-    message = forms.CharField(label='', widget=forms.Textarea(attrs={'placeholder': 'Your entry here...'}))
+    title = forms.CharField(label='Title', widget=forms.TextInput(attrs={'placeholder': 'title', 'class': 'sizeform'}))
+    content = forms.CharField(label='Description', widget=forms.Textarea(attrs={'placeholder': 'Your description here...'}))
 
 
 def index(request):
@@ -50,8 +63,32 @@ def search(request):
             "compare_word": compare_word,
             "capitalize": capitalize
         })
+    else:
+        return redirect("encyclopedia:index")
 
 def newPage(request):
+    if request.method == "POST":
+        title = request.POST.get('title').capitalize()
+        content = request.POST.get('content')
+        print(f"{title}, {content}")
+
+        danger = 50
+        compareTitles = util.list_entries()
+
+        if title in compareTitles:
+            return render(request, "encyclopedia/newPage.html", {
+                "form": NewPageForm,
+                "message": messages.add_message(request, danger, 'Error: an entry with the same name already exists. Cannot overrride data. Entry not saved.')
+            })
+
+        else:
+            util.save_entry(title,content)
+            messages.add_message(request, messages.SUCCESS, 'Entry successfully saved. Thank you for your contribution!!')
+            return HttpResponseRedirect(reverse("encyclopedia:query", args=(title,))) 
+            # use HttpResponseRedirect instead of redirect to also redirect the flash message.
+            # args=(title,) allows to pass the title parameter to the query view to open the new page correctly
+
     return render(request, "encyclopedia/newPage.html", {
         "form": NewPageForm
     })
+        
